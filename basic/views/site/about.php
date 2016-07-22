ada03d07468e64717682648ee50464125ac689a8
Alexandr Tikhonov <t9101029991@gmail.com>
2016-07-22 12:30:58 +0300

    correct calculator template and user scalable no

--- basic/views/site/about.php
+++ basic/views/site/about.php
@@ -56,19 +56,11 @@ $this->params['breadcrumbs'][] = $this->title;
   </div>
 </div>
 
-     <div id="loginbox" style="margin-top:50px;" class="col-md-12 ">                    
-            <div class="panel panel-primary" >
-                    <div class="panel-heading">
-                        <div class="panel-title">Что хотите бронировать?</div>
-                        
-                    </div>     
 
-                    <div style="padding-top:30px" class="panel-body" >
 
-                        <div style="display:none" id="login-alert" class="alert alert-danger col-sm-12"></div>
-                            
-
-                          <div class="col-sm-6 col-md-4">
+<div class="row">
+	
+	<div class="col-md-4">
 						    <div class="thumbnail">
 						    	<div class="color-img-wrapper">
 						      		<img class='color-img-400 color-img img-responsive' src="/images/460grey.JPG" alt="...">
@@ -107,7 +99,7 @@ $this->params['breadcrumbs'][] = $this->title;
 						    </div>
 						  </div>
 
-						  <div class="col-sm-6 col-md-4">
+						  <div class="col-md-4">
 						    <div class="thumbnail">
 						      	<div class="color-img-wrapper">
 						      		<img class='color-img-460 color-img img-responsive' src="/images/460grey.JPG" alt="...">
@@ -146,7 +138,137 @@ $this->params['breadcrumbs'][] = $this->title;
 						    </div>
 						  </div>
 
-						  <div class="col-sm-6 col-md-4">
+						  <div class="col-md-4">
+						    <div class="thumbnail">
+						    	<div class="color-img-wrapper">
+						      		<img class='color-img-520 color-img img-responsive' src="/images/520grey.JPG" alt="...">
+						      	</div>
+						      <div class="caption">
+						        <h3>Автобокс "Satellite 520"</h3>
+						        <table class="table table-striped">
+						        	<tr>
+						        		<td>Цвет</td>
+						        		<td>
+						        			<span class='color color-silver' id='520' data-color='grey'></span>
+						        			<span class='color color-white' id='520' data-color='white'></span>
+						        			<span class='color color-black' id='520' data-color='black'></span>
+						        		</td>
+						        	</tr>
+						        	<tr>
+						        		<td>Объем</td>
+						        		<td>520 литров</td>
+						        	</tr>
+						        	<tr>
+						        		<td>Габариты</td>
+						        		<td>195x78x43</td>
+						        	</tr>
+						        	<tr>
+						        		<td>Масса</td>
+						        		<td>15 кг</td>
+						        	</tr>
+						        	<tr>
+						        		<td>Грузоподъемность</td>
+						        		<td>до 80 кг.</td>
+						        	</tr>
+						        </table>
+						        
+						        <p><span class="btn btn-default btn-color" role="button">Выбрать</span></p>
+						      </div>
+						    </div>
+						  </div>
+</div>
+
+     <div id="loginbox" style="margin-top:50px; display:none" class="col-md-12 ">                    
+            <div class="panel panel-primary" >
+                    <div class="panel-heading">
+                        <div class="panel-title">Что хотите бронировать?</div>
+                        
+                    </div>     
+
+                    <div style="padding-top:30px" class="panel-body" >
+
+                        <div style="display:none" id="login-alert" class="alert alert-danger col-sm-12"></div>
+                            
+
+                          <div class="col-md-4">
+						    <div class="thumbnail">
+						    	<div class="color-img-wrapper">
+						      		<img class='color-img-400 color-img img-responsive' src="/images/460grey.JPG" alt="...">
+						      	</div>
+						      <div class="caption">
+						        <h3>Автобокс "Satellite 400"</h3>
+						        <table class="table table-striped">
+						        	<tr>
+						        		<td>Цвет</td>
+						        		<td>
+						        			<span class='color color-silver' id='400' data-color='grey'></span>
+						        			<span class='color color-white' id='400' data-color='white'></span>
+						        			<span class='color color-black' id='400' data-color='black'></span>
+						        		</td>
+						        	</tr>
+						        	<tr>
+						        		<td>Объем</td>
+						        		<td>400 литров</td>
+						        	</tr>
+						        	<tr>
+						        		<td>Габариты</td>
+						        		<td>175x82x45</td>
+						        	</tr>
+						        	<tr>
+						        		<td>Масса</td>
+						        		<td>13 кг</td>
+						        	</tr>
+						        	<tr>
+						        		<td>Грузоподъемность</td>
+						        		<td>до 60 кг.</td>
+						        	</tr>
+						        </table>
+						        
+						        <p><span class="btn btn-default btn-color" role="button">Выбрать</span></p>
+						      </div>
+						    </div>
+						  </div>
+
+						  <div class="col-md-4">
+						    <div class="thumbnail">
+						      	<div class="color-img-wrapper">
+						      		<img class='color-img-460 color-img img-responsive' src="/images/460grey.JPG" alt="...">
+						      	</div>
+						      <div class="caption">
+						        <h3>Автобокс "Satellite 460"</h3>
+						        <table class="table table-striped">
+						        	<tr>
+						        		<td>Цвет</td>
+						        		<td>
+						        			<span class='color color-silver' id='460' data-color='grey'></span>
+						        			<span class='color color-white' id='460' data-color='white'></span>
+						        			<span class='color color-black' id='460' data-color='black'></span>
+						        		</td>
+						        	</tr>
+						        	<tr>
+						        		<td>Объем</td>
+						        		<td>460 литров</td>
+						        	</tr>
+						        	<tr>
+						        		<td>Габариты</td>
+						        		<td>175x82x45</td>
+						        	</tr>
+						        	<tr>
+						        		<td>Масса</td>
+						        		<td>13 кг</td>
+						        	</tr>
+						        	<tr>
+						        		<td>Грузоподъемность</td>
+						        		<td>до 60 кг.</td>
+						        	</tr>
+						        </table>
+						        
+						        <p><span class="btn btn-default btn-color" role="button">Выбрать</span></p>
+						      </div>
+						    </div>
+						  </div>
+
+						  <div class="col-md-4">
 						    <div class="thumbnail">
 						    	<div class="color-img-wrapper">
 						      		<img class='color-img-520 color-img img-responsive' src="/images/520grey.JPG" alt="...">
