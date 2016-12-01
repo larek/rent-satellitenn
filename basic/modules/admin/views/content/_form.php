8e409066444eaccb0888692d7e772db538788b35
Alexandr Tikhonov <t9101029991@gmail.com>
2016-12-01 13:02:01 +0300

    add fontselectsize and fontcolor in tynemce in admin

--- basic/modules/admin/views/content/_form.php
+++ basic/modules/admin/views/content/_form.php
@@ -34,9 +34,10 @@ use dosamigos\tinymce\TinyMce;
             'plugins' => [
                 "advlist autolink lists link charmap print preview anchor",
                 "searchreplace visualblocks code fullscreen",
-                "insertdatetime media table contextmenu paste"
+                "insertdatetime media table contextmenu paste",
+                "textcolor colorpicker"
             ],
-            'toolbar' => "undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
+            'toolbar' => "fontsizeselect | forecolor backcolor | undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
         ]
     ]);?>
 
@@ -47,9 +48,10 @@ use dosamigos\tinymce\TinyMce;
             'plugins' => [
                 "advlist autolink lists link charmap print preview anchor",
                 "searchreplace visualblocks code fullscreen",
-                "insertdatetime media table contextmenu paste"
+                "insertdatetime media table contextmenu paste",
+                "textcolor colorpicker"
             ],
-            'toolbar' => "undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
+            'toolbar' => "fontsizeselect | forecolor backcolor | undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
         ]
     ]);?>
 
